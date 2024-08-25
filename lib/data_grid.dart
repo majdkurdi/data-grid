aa5df217b71f703feca88155667fd642ed928919
Majd Kurdi <majdkurdi1998@gmail.com>
2024-08-26 00:05:36 +0200

    .

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -581,8 +581,10 @@ class _XtraDataGridState extends State<XtraDataGrid> {
               },
         onLongPress: () => widget.onLongPress?.call(row),
         onTapDown: (_) async {
-          focusNode.requestFocus();
+          Future.delayed(Duration(milliseconds: 100), () {
+            focusNode.requestFocus();
           print(focusNode.hasFocus);
+          });
           final oldCell = currentCell;
 
           currentCell = index;
