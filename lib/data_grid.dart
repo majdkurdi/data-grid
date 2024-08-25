ace214e799d4f0607eed30ba1cdc3aedc0a72d7f
Majd Kurdi <majdkurdi1998@gmail.com>
2024-08-26 00:18:54 +0200

    .

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -83,7 +83,7 @@ class _XtraDataGridState extends State<XtraDataGrid> {
   RowColumnIndex currentCell = RowColumnIndex(0, 0);
   bool editMode = false;
   // bool columnDragging = false;
-  late final focusNode = widget.focusNode ?? FocusNode();
+  late final focusNode = widget.focusNode ?? FocusNode(debugLabel: widget.key.toString());
   final scrollController = AutoScrollController();
   final headerController = AutoScrollController();
   final verticalController = AutoScrollController();
@@ -983,7 +983,7 @@ class _XtraDataGridState extends State<XtraDataGrid> {
             );
           }
           focusNode.addListener(() {
-  debugPrint('FocusNode has focus: ${focusNode.hasFocus}');
+  debugPrint('${focusNode.debugLabel} has focus: ${focusNode.hasFocus}');
 });
 
           return KeyboardListener(
