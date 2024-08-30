9ca8019e67e4f42bf3a217e35efba3b4b006d231
Majd Kurdi <majdkurdi1998@gmail.com>
2024-08-30 14:41:45 +0200

    .

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -735,57 +735,65 @@ class _XtraDataGridState extends State<XtraDataGrid> {
                     _headerCell(MyGridColumn(
                         label: '', columnName: 'index', width: 30)),
                     Expanded(
-                      child: ListView(
-                          physics: const ClampingScrollPhysics(),
-                          controller: headerController,
-                          // dragStartBehavior: DragStartBehavior.down,
-                          // onReorderStart: (index) => print(index),
-                          scrollDirection: Axis.horizontal,
-                          // buildDefaultDragHandles: false,
-                          // physics: const NeverScrollableScrollPhysics(),
-                          // shrinkWrap: true,
-                          // onReorderStart: (_) {
-                          //   print('start');
-                          //   setState(() => columnDragging = true);
-                          // },
-                          // onReorderEnd: (_) =>
-                          //     setState(() => columnDragging = false),
-                          // onReorder: (oldI, newI) {
-                          //   if (widget.onColumnsReorder != null) {
-                          //     widget.onColumnsReorder!(oldI, newI);
-                          //   }
-                          // },
-                          children: widget.columns.map<Widget>((e) {
-                            final i = widget.columns.indexOf(e);
-                            return AutoScrollTag(
-                              index: i,
-                              controller: headerController,
-                              key: ValueKey(i),
-                              child: MouseRegion(
-                                cursor: widget.onColumnsReorder != null
-                                    ? SystemMouseCursors.grab
-                                    : SystemMouseCursors.basic,
-                                // key: ValueKey(e.columnName),
-                                child: Builder(
-                                  builder: (ct) {
-                                    return !scrollableGrid &&
-                                            widget.columns.last == e
-                                        ? SizedBox(
-                                            height: widget.rowHeight,
-                                            width: spaceForLastColumn - 30,
-                                            child: _headerCell(e),
-                                          )
-                                        : _headerCell(e);
-                                  },
+                      child: NotificationListener(
+                        onNotification: (ScrollNotification scrollInfo) {
+                          if (scrollController.hasClients && headerController.position.pixels != scrollInfo.metrics.pixels) {
+                            scrollController.jumpTo(scrollInfo.metrics.pixels);
+                          }
+                          return true;
+                        },
+                        child: ListView(
+                            physics: const ClampingScrollPhysics(),
+                            controller: headerController,
+                            // dragStartBehavior: DragStartBehavior.down,
+                            // onReorderStart: (index) => print(index),
+                            scrollDirection: Axis.horizontal,
+                            // buildDefaultDragHandles: false,
+                            // physics: const NeverScrollableScrollPhysics(),
+                            // shrinkWrap: true,
+                            // onReorderStart: (_) {
+                            //   print('start');
+                            //   setState(() => columnDragging = true);
+                            // },
+                            // onReorderEnd: (_) =>
+                            //     setState(() => columnDragging = false),
+                            // onReorder: (oldI, newI) {
+                            //   if (widget.onColumnsReorder != null) {
+                            //     widget.onColumnsReorder!(oldI, newI);
+                            //   }
+                            // },
+                            children: widget.columns.map<Widget>((e) {
+                              final i = widget.columns.indexOf(e);
+                              return AutoScrollTag(
+                                index: i,
+                                controller: headerController,
+                                key: ValueKey(i),
+                                child: MouseRegion(
+                                  cursor: widget.onColumnsReorder != null
+                                      ? SystemMouseCursors.grab
+                                      : SystemMouseCursors.basic,
+                                  // key: ValueKey(e.columnName),
+                                  child: Builder(
+                                    builder: (ct) {
+                                      return !scrollableGrid &&
+                                              widget.columns.last == e
+                                          ? SizedBox(
+                                              height: widget.rowHeight,
+                                              width: spaceForLastColumn - 30,
+                                              child: _headerCell(e),
+                                            )
+                                          : _headerCell(e);
+                                    },
+                                  ),
                                 ),
-                              ),
-                            );
-                          }).toList()
-                          // ..insert(
-                          //     0,
-                          //     _headerCell(MyGridColumn(
-                          //         label: '', columnName: 'index', width: 30))),
-                          ),
+                              );
+                            }).toList()
+                            // ..insert(
+                            //     0,
+                            //     _headerCell(MyGridColumn(
+                            //         label: '', columnName: 'index', width: 30))),
+                            ),
+                      ),
                     ),
                   ],
                 ),
@@ -795,40 +803,49 @@ class _XtraDataGridState extends State<XtraDataGrid> {
                   AutoScrollController? bindingController]) =>
               Column(
                 children: [
-                  Expanded(
-                    child: SizedBox(
-                      width: scrollableGrid ? requiredWidth : null,
-                      child: ListView.builder(
-                        prototypeItem: SizedBox(height: widget.rowHeight),
-                        // shrinkWrap: true,
-                        physics: const ClampingScrollPhysics(),
-                        controller: controller ?? verticalController,
-                        itemCount: rows.length,
-                        itemBuilder: (ctx, i) => AutoScrollTag(
+                  NotificationListener(
+                    onNotification: (ScrollNotification scrollInfo) {
+                      if ((bindingController ?? indexesController).hasClients && (bindingController ?? indexesController).position.pixels != scrollInfo.metrics.pixels) {
+                        (bindingController ?? indexesController)
+                            .jumpTo(scrollInfo.metrics.pixels);
+                      }
+                      return true;
+                    },
+                    child: Expanded(
+                      child: SizedBox(
+                        width: scrollableGrid ? requiredWidth : null,
+                        child: ListView.builder(
+                          prototypeItem: SizedBox(height: widget.rowHeight),
+                          // shrinkWrap: true,
+                          physics: const ClampingScrollPhysics(),
                           controller: controller ?? verticalController,
-                          key: ValueKey(i),
-                          index: i,
-                          child: SizedBox(
-                            height: widget.rowHeight,
-                            child: Row(
-                              children: widget.columns.map((column) {
-                                final cell = rows[i]
-                                    .getCells()
-                                    // [widget.columns.indexOf(column)];
-                                    .firstWhere((c) =>
-                                        c.columnName == column.columnName);
-                                final index = RowColumnIndex(
-                                    i, widget.columns.indexOf(column));
-                                final k = GlobalKey();
-                                cellsKeys[column.columnName]?.add(k);
-                                return !scrollableGrid &&
-                                        widget.columns.last == column
-                                    ? Expanded(
-                                        child: _gridCell(
-                                            index, rows[i], column, cell, k))
-                                    : _gridCell(
-                                        index, rows[i], column, cell, k);
-                              }).toList(),
+                          itemCount: rows.length,
+                          itemBuilder: (ctx, i) => AutoScrollTag(
+                            controller: controller ?? verticalController,
+                            key: ValueKey(i),
+                            index: i,
+                            child: SizedBox(
+                              height: widget.rowHeight,
+                              child: Row(
+                                children: widget.columns.map((column) {
+                                  final cell = rows[i]
+                                      .getCells()
+                                      // [widget.columns.indexOf(column)];
+                                      .firstWhere((c) =>
+                                          c.columnName == column.columnName);
+                                  final index = RowColumnIndex(
+                                      i, widget.columns.indexOf(column));
+                                  final k = GlobalKey();
+                                  cellsKeys[column.columnName]?.add(k);
+                                  return !scrollableGrid &&
+                                          widget.columns.last == column
+                                      ? Expanded(
+                                          child: _gridCell(
+                                              index, rows[i], column, cell, k))
+                                      : _gridCell(
+                                          index, rows[i], column, cell, k);
+                                }).toList(),
+                              ),
                             ),
                           ),
                         ),
@@ -843,25 +860,32 @@ class _XtraDataGridState extends State<XtraDataGrid> {
                   AutoScrollController? bindingController]) =>
               Column(
                 children: [
-                  Expanded(
-                    child: SizedBox(
-                      width: 30,
-                      child: ListView.builder(
-                        prototypeItem: SizedBox(height: widget.rowHeight),
-                        controller: controller ?? indexesController,
-                        itemCount: rows.length,
-                        itemBuilder: (ctx, i) => Container(
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(width: 0.3, color: Colors.grey),
-                              color: Get.theme.colorScheme.tertiary),
-                          height: widget.rowHeight,
-                          width: 30,
-                          child: Center(
-                            child: FittedBox(
-                              child: Text(
-                                (i + 1).toString(),
-                                style: widget.headerStyle,
+                  NotificationListener(
+                    onNotification: (ScrollNotification scrollInfo) {
+                      (bindingController ?? verticalController)
+                          .jumpTo(scrollInfo.metrics.pixels);
+                      return true;
+                    },
+                    child: Expanded(
+                      child: SizedBox(
+                        width: 30,
+                        child: ListView.builder(
+                          prototypeItem: SizedBox(height: widget.rowHeight),
+                          controller: controller ?? indexesController,
+                          itemCount: rows.length,
+                          itemBuilder: (ctx, i) => Container(
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 0.3, color: Colors.grey),
+                                color: Get.theme.colorScheme.tertiary),
+                            height: widget.rowHeight,
+                            width: 30,
+                            child: Center(
+                              child: FittedBox(
+                                child: Text(
+                                  (i + 1).toString(),
+                                  style: widget.headerStyle,
+                                ),
                               ),
                             ),
                           ),
@@ -984,13 +1008,23 @@ class _XtraDataGridState extends State<XtraDataGrid> {
                                 if (groupByColumn == null)
                                   indexes(widget.source.rows),
                                 Expanded(
-                                  child: SingleChildScrollView(
-                                    physics: const ClampingScrollPhysics(),
-                                    controller: scrollController,
-                                    scrollDirection: Axis.horizontal,
-                                    child: groupByColumn == null
-                                        ? grid(widget.source.rows)
-                                        : stackedGrid(),
+                                  child: NotificationListener(
+                                    onNotification:
+                                        (ScrollNotification scrollInfo) {
+                                      if (headerController.hasClients) {
+                                        headerController
+                                            .jumpTo(scrollInfo.metrics.pixels);
+                                      }
+                                      return true;
+                                    },
+                                    child: SingleChildScrollView(
+                                      physics: const ClampingScrollPhysics(),
+                                      controller: scrollController,
+                                      scrollDirection: Axis.horizontal,
+                                      child: groupByColumn == null
+                                          ? grid(widget.source.rows)
+                                          : stackedGrid(),
+                                    ),
                                   ),
                                 ),
                               ],
