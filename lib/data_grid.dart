8af1393b7b0868dde346d72cdb4e42994fea90fe
Majd Kurdi <majdkurdi1998@gmail.com>
2024-08-30 05:47:28 +0200

    .

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -989,43 +989,54 @@ class _XtraDataGridState extends State<XtraDataGrid> {
             focusNode: focusNode,
             child: scrollableGrid
                 ? Scrollbar(
-                  trackVisibility: true,
+                  // trackVisibility: true,
+                  interactive: true,
                     controller: scrollController,
                     thumbVisibility: true,
                     scrollbarOrientation: ScrollbarOrientation.bottom,
-                    child: Column(
-                      children: [
-                        headers(),
-                        Expanded(
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (groupByColumn == null)
-                                indexes(widget.source.rows),
-                              Expanded(
-                                child: NotificationListener(
-                                  onNotification:
-                                      (ScrollNotification scrollInfo) {
-                                    if (headerController.hasClients) {
-                                      headerController
-                                          .jumpTo(scrollInfo.metrics.pixels);
-                                    }
-                                    return true;
-                                  },
-                                  child: SingleChildScrollView(
-                                    physics: const ClampingScrollPhysics(),
-                                    controller: scrollController,
-                                    scrollDirection: Axis.horizontal,
-                                    child: groupByColumn == null
-                                        ? grid(widget.source.rows)
-                                        : stackedGrid(),
+                    child: Scrollbar(
+                      // trackVisibility: true,
+                      controller: verticalController,
+                      thumbVisibility: verticalController.hasClients &&
+                          groupByColumn == null,
+                      interactive: true,
+                      scrollbarOrientation: arabicLocale
+                          ? ScrollbarOrientation.left
+                          : ScrollbarOrientation.right,
+                      child: Column(
+                        children: [
+                          headers(),
+                          Expanded(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (groupByColumn == null)
+                                  indexes(widget.source.rows),
+                                Expanded(
+                                  child: NotificationListener(
+                                    onNotification:
+                                        (ScrollNotification scrollInfo) {
+                                      if (headerController.hasClients) {
+                                        headerController
+                                            .jumpTo(scrollInfo.metrics.pixels);
+                                      }
+                                      return true;
+                                    },
+                                    child: SingleChildScrollView(
+                                      physics: const ClampingScrollPhysics(),
+                                      controller: scrollController,
+                                      scrollDirection: Axis.horizontal,
+                                      child: groupByColumn == null
+                                          ? grid(widget.source.rows)
+                                          : stackedGrid(),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   )
                 : Scrollbar(
