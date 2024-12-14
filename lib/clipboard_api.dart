c0be2c8c1bc168c176a1f2144a971fc590c4d3ed
Majd Kurdi <majdkurdi1998@gmail.com>
2024-12-14 12:56:36 +0100

    .

--- lib/clipboard_api.dart
+++ lib/clipboard_api.dart
@@ -21,7 +21,7 @@ Future<List<List<String>>> parseExcelClipboard() async {
     return [];
   }
   final rows =
-      clipboardText.split('\n').map((e) => e.replaceAll('\u{1F972}', '')).toList();
+      clipboardText.split('\n').map((e) => e.replaceAll('\u{000D}', '')).toList();
   final List<List<String>> parsedData = rows
       .map((row) => row.split('\t').map((e) => e.replaceAll('\t', '')).toList())
       .toList();
