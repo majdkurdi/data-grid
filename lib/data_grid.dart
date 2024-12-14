7bf1cd702bf3943b3a3f2bce831a0ae5c07e4b36
Majd Kurdi <majdkurdi1998@gmail.com>
2024-12-14 13:44:06 +0100

    .

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -397,7 +397,6 @@ class _XtraDataGridState extends State<XtraDataGrid> {
           widget.columns[cellIndex.columnIndex]);
     } else if (clipboardContent is List<List<String>> &&
         widget.source.rowFromClipboard != null) {
-          print(cellIndex.rowIndex);
       for(var i in widget.source.rows.getRange(
           cellIndex.rowIndex, cellIndex.rowIndex + clipboardContent.length +1)){
             widget.source.deleteRow(i);
