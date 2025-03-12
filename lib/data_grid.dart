30311e6f9e7238d8687a46a6471ed9fe4c85c27c
Majd Kurdi <majdkurdi1998@gmail.com>
2025-03-12 05:21:47 +0100

    .

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -119,8 +119,8 @@ class _XtraDataGridState extends State<XtraDataGrid> {
   void sortGridAZ(MyGridColumn column) {
     if (column.compareValuesForSort == null) return;
     widget.source.rows.sort((a, b) => column.compareValuesForSort!(
-        a.cells.firstWhere((e) => e.columnName == column.columnName),
-        b.cells.firstWhere((e) => e.columnName == column.columnName)));
+        a.cells.firstWhere((e) => e.columnName == column.columnName).value,
+        b.cells.firstWhere((e) => e.columnName == column.columnName).value));
     setState(() {});
   }
 
